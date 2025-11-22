8e49fb94f59d96307607c8ce9cbf925e7d5f5b7d
Syed-shan46 <syedshan093@gmail.com>
2025-11-22 14:13:48 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const corsOptions = {
+  origin: 'http://localhost:5173', // Adjust this to your client's origin
+};
+app.use(cors(corsOptions));
+
+app.get('/api', (req, res) => {
+  res.json({fruits: ['apple', 'banana', 'cherry']});
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
