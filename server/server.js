5b575d58275eba52c1a708740d752e853d4280af
Hira Outsourcing <hira@Hiras-iMac.lan>
2025-11-30 23:17:45 +0530

    trust proxy added

--- server/server.js
+++ server/server.js
@@ -32,6 +32,8 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set("trust proxy", 1)
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
